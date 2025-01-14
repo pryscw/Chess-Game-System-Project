3ab59f3db542222f6f841caebc1125a374a08633
Priscilla Oliveira <oliveirapriscilla524@gmail.com>
2025-01-14 20:31:40 -0300

    Projeto criado

---
+++ Chess Game/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Globalization;
+using System.Collections.Generic;
+
+namespace Chess_Game
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
