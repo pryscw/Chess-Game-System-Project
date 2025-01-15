ab27f76daae836cbe0f84f1b5016eca7328b7f48
Priscilla Oliveira <oliveirapriscilla524@gmail.com>
2025-01-15 12:53:38 -0300

    Melhorando a implementação do método imprimirtabuleiro

--- Chess Game/Tela.cs
+++ Chess Game/Tela.cs
@@ -1,5 +1,5 @@
 ﻿using tabuleiro;
-    
+
 namespace Chess_Game
 {
     internal class Tela
@@ -8,18 +8,38 @@ namespace Chess_Game
         {
             for (int i = 0; i < tab.linhas; i++)
             {
-                for(int j = 0; j < tab.colunas; j++)
+                Console.Write(8 - i + " ");
+                for (int j = 0; j < tab.colunas; j++)
                 {
-                    if(tab.peca(i, j) == null)
+                    if (tab.peca(i, j) == null)
                     {
                         Console.Write("- ");
-                    } else
+                    }
+                    else
                     {
-                        Console.Write(tab.peca(i, j) + " ");
+                        imprimirPeca(tab.peca(i, j));
+                        Console.Write(" ");
                     }
                 }
                 Console.WriteLine();
             }
+            Console.WriteLine("  a b c d e f g h ");
+        }
+
+        public static void imprimirPeca(Peca peca)
+        {
+            if (peca.cor == Cor.Branca)
+            {
+                Console.Write(peca);
+            }
+            else
+            {
+                ConsoleColor aux = Console.ForegroundColor;
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.Write(peca);
+                Console.ForegroundColor = aux;
+            }
+            
         }
     }
 }
